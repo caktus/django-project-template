1daa3df195842d254c33493b9e90e4bee298d669
mlavin <markdlavin@gmail.com>
2012-08-02 20:55:24 -0400

    Serve MEDIA_ROOT directory under MEDIA_URL when DEBUG is True.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -8,3 +9,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^{0}(?P<path>.*)$'.format(settings.MEDIA_URL.lstrip('/')),
+            'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}
+        ),
+   )
