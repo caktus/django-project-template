ad5e51ef058879d10de95df813bfaf71e76fe622
mlavin <markdlavin@gmail.com>
2012-08-25 15:11:13 -0400

    Update to use static url pattern helper.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 
@@ -8,12 +9,4 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-)
-
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^{0}(?P<path>.*)$'.format(settings.MEDIA_URL.lstrip('/')),
-            'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}
-        ),
-   )
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
