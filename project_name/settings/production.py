8c0be5201d9177ee9729423cc8c34338db08ecd5
Colin Copeland <copelco@caktusgroup.com>
2013-07-01 10:31:42 -0400

    add example BROKER_URL setting

--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -11,3 +11,6 @@ STATIC_ROOT = os.path.join(PUBLIC_ROOT, 'static')
 MEDIA_ROOT = os.path.join(PUBLIC_ROOT, 'media')
 
 EMAIL_SUBJECT_PREFIX = '[{{ project_name|title }} Prod] '
+
+# Uncomment if using async worker configuration
+# BROKER_URL = 'amqp://{{ project_name }}:%s@127.0.0.1:5672/{{ project_name }}_production' % os.environ['BROKER_PASSWORD']
