52f76dca27006de0de97c5609fd55fcef98d912b
Mark Lavin <mlavin@caktusgroup.com>
2014-01-03 09:19:44 -0500

    Simplify default dev settings for new version of debug toolbar.

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -5,10 +5,6 @@ from {{ project_name }}.settings.base import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
 INSTALLED_APPS += (
     'debug_toolbar',
 )
@@ -16,10 +12,6 @@ INSTALLED_APPS += (
 INTERNAL_IPS = ('127.0.0.1', )
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False
-}
 
 SOUTH_TESTS_MIGRATE = True
 
