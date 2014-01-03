53d50dbeec399c3806283619375050f14f717081
Mark Lavin <mlavin@caktusgroup.com>
2014-01-03 11:44:26 -0500

    Update settings to match the current configurations. Pass the allowed host domain which is set by the load balancer to the ALLOWED_HOSTS setting.

--- project_name/settings/staging.py
+++ project_name/settings/staging.py
@@ -4,8 +4,10 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES['default']['NAME'] = '{{ project_name }}_staging'
+DATABASES['default']['USER'] = '{{ project_name }}_staging'
+DATABASES['default']['PASSWORD'] = os.environ['DB_PASSWORD']
 
-PUBLIC_ROOT = '/var/www/{{ project_name }}-staging/public/'
+PUBLIC_ROOT = '/var/www/{{ project_name }}/public/'
 
 STATIC_ROOT = os.path.join(PUBLIC_ROOT, 'static')
 
@@ -26,7 +28,9 @@ SESSION_COOKIE_SECURE = True
 
 SESSION_COOKIE_HTTPONLY = True
 
-ALLOWED_HOSTS = ()
+ALLOWED_HOSTS = (
+    os.environ['ALLOWED_HOST'],
+)
 
 # Uncomment if using celery worker configuration
-# BROKER_URL = 'amqp://{{ project_name }}:%s@127.0.0.1:5672/{{ project_name }}_staging' % os.environ['BROKER_PASSWORD']
+# BROKER_URL = 'amqp://{{ project_name }}_staging:%s@127.0.0.1:5672/{{ project_name }}_staging' % os.environ['BROKER_PASSWORD']
