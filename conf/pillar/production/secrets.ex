331f95a61d5cfeb2f8228ab3a6272f3ed26df196
Mark Lavin <mlavin@caktusgroup.com>
2014-01-03 11:35:47 -0500

    Update pillars and examples for relevant available configurations/settings.

--- conf/pillar/production/secrets.ex
+++
@@ -1,5 +0,0 @@
-# This file should be renamed to secrets.sls
-secrets:
-  DB_PASSWORD: XXXXXX
-# Uncomment if using celery worker configuration
-#   BROKER_PASSWORD: XXXXXX
--- conf/pillar/secrets.ex
+++ conf/pillar/secrets.ex
@@ -1,4 +1,4 @@
-# This file should be renamed to secrets.sls
+# This file should be renamed to secrets.sls in the relevant environment directory
 secrets:
   DB_PASSWORD: XXXXXX
 # Uncomment if using celery worker configuration
@@ -7,3 +7,8 @@ secrets:
 # Uncomment and update username/password to enable HTTP basic auth
 # http_auth:
 #   username: password
+
+github_deploy_key: |
+  -----BEGIN RSA PRIVATE KEY-----
+  foobar
+  -----END RSA PRIVATE KEY-----
