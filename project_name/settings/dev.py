c10eb5a809ae0bf37cb85da1476a13c4e24af32e
Vinod Kurup <vinod@kurup.com>
2014-02-20 10:00:00 -0500

    Remove redundant settings

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -22,11 +22,6 @@ COMPRESS_ENABLED = False
 
 # Special test settings
 if 'test' in sys.argv:
-    CELERY_ALWAYS_EAGER = True
-    CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
-
-    COMPRESS_ENABLED = False
-
     COMPRESS_PRECOMPILERS = ()
 
     PASSWORD_HASHERS = (
