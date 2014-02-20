bddee96131be7604b03f09e3865185ac35a3e67f
Vinod Kurup <vinod@kurup.com>
2014-02-20 09:41:08 -0500

    Set CELERY_EAGER_PROPAGATES_EXCEPTIONS

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -16,12 +16,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SOUTH_TESTS_MIGRATE = True
 
 CELERY_ALWAYS_EAGER = True
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
 
 COMPRESS_ENABLED = False
 
 # Special test settings
 if 'test' in sys.argv:
     CELERY_ALWAYS_EAGER = True
+    CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
 
     COMPRESS_ENABLED = False
 
