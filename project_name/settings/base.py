921dfce2fbe765fea33d817966b016b86048f576
Vinod Kurup <vinod@kurup.com>
2015-09-15 10:50:38 -0400

    Add locale middleware

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
