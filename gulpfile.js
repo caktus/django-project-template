363157af1064eb76a0c20eddea233577b17f3165
Calvin Spealman <calvin@caktusgroup.com>
2016-04-14 22:31:30 -0400

    Rework how the gulp tasks are pulled in externally so we can inject our copy of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,3 @@
-var gulp = require('gulp');
-var tasks = require('caktus-gulp-tasks').tasks;
-
-gulp.task('test', tasks.test);
-gulp.task('build', tasks.build);
-gulp.task('default', tasks.default);
+var tasks = require('@caktus/gulp-tasks')(require('gulp'), {
+    project_name: "{{ project_name }}",
+});
