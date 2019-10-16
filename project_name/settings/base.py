fabaf598bc21caffd0867d5b522887845dce7706
Christopher Dixon <cdixon@caktusgroup.com>
2019-10-16 09:31:33 -0400

    basic configuration for integrating wagtail into a django project

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -32,6 +32,19 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.humanize',
     'django.contrib.sitemaps',
+    'wagtail.contrib.forms',
+    'wagtail.contrib.redirects',
+    'wagtail.embeds',
+    'wagtail.sites',
+    'wagtail.users',
+    'wagtail.snippets',
+    'wagtail.documents',
+    'wagtail.images',
+    'wagtail.search',
+    'wagtail.admin',
+    'wagtail.core',
+    'modelcluster',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -43,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'wagtail.core.middleware.SiteMiddleware',
+    'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
 
@@ -71,6 +86,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 
+# Displayed on the main dashboard of the Wagtail admin
+WAGTAIL_SITE_NAME = '{{ project_name }}'
 
 # Database
 # https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
--- project_name/urls.py
+++ project_name/urls.py
@@ -1,5 +1,7 @@
 """{{ project_name }} URL Configuration
 
+| -- Django URL Patterns -- |
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/{{ docs_version }}/topics/http/urls/
 Examples:
@@ -12,16 +14,35 @@ Class-based views
 Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+--------------------------------------------------------------------------------
+| -- Wagtail URL Patterns for Wagtail Only Projects -- |
+
+urlpatterns = [
+    ...
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+    re_path(r'^admin/', include(wagtailadmin_urls)),
+    # must be placed at the end of the urlpattern list (wagtail handles the entire url space)
+    re_path(r'', include(wagtail_urls)),
+]
+
+
 """
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import url, include, re_path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.core import urls as wagtail_urls
+
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^admin/', admin.site.urls),
+    re_path(r'^cms/', include(wagtailadmin_urls)),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+    re_path(r'^pages/', include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
